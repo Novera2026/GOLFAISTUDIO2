8785c3950276356c38bc71513dccece812835b56
Novera2026 <imouse2016.love@gmail.com>
2026-06-03 01:07:16 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
       plugins: [react(), tailwindcss()],
       define: {
     'process.env.API_KEY': JSON.stringify(env.GOOGLE_API_KEY),
-    'process.env.GEMINI_API_KEY': JSON.stringify(env.GOOGLE_API_KEY)
+    'process.env.GOOGLE_API_KEY': JSON.stringify(env.GOOGLE_API_KEY)
   },
       resolve: {
         alias: {
